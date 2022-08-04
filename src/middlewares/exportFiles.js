78c660a32fb55df49f7158fac0f69b66e00e21a9
Bernardo Casanova <bernardocasanova@gmail.com>
2022-08-04 03:30:58 -0300

    Criacao e configuracao do middleware de exportacao para o csv

---
+++ src/middlewares/exportFiles.js
@@ -0,0 +1,70 @@
+import * as json2csv from 'json2csv';
+import * as fs from 'fs';
+import { resolve } from 'path';
+
+class ExportFiles {
+  /**
+   * Generate the requests per consumer report
+   *
+   * @return void
+   */
+  async exportRequestsByConsumer(requests) {
+    const fields = [
+      'host',
+      'quantity',
+    ];
+    const filename = 'RequestsByConsumer.csv';
+    await this.createCsv(requests, fields, filename);
+  }
+
+  /**
+   * Generate the requests per service report
+   *
+   * @return void
+   */
+  async exportRequestsByService(requests) {
+    const fields = [
+      'service_name',
+      'quantity',
+    ];
+    const filename = 'RequestsByService.csv';
+    await this.createCsv(requests, fields, filename);
+  }
+
+  /**
+   * Generate average time per service
+   *
+   * @return void
+   */
+  async exportAverageTimeService(requests) {
+    const fields = [
+      'service_name',
+      'latency_request',
+      'latency_kong',
+      'latency_proxy',
+    ];
+    const filename = 'AverageTimeService.csv';
+    await this.createCsv(requests, fields, filename);
+  }
+
+  /**
+   * @param array requests with database records
+   * @param array fields headers to csv file
+   * @param string $filename File name
+   *
+   * @return string report .csv file created location
+   */
+  async createCsv(requests, fields, filename) {
+    const opts = { fields };
+    const csv = await json2csv.parse(requests, opts);
+    const filepath = resolve(__dirname, '../', 'extras', 'exports', filename);
+
+    fs.writeFile(filepath, csv, (err) => {
+      if (err) throw err;
+    });
+
+    return filepath;
+  }
+}
+
+export default new ExportFiles();
