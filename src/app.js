940b15f7a2be47523716014ce313e289c5f7e7e8
Bernardo Casanova <bernardocasanova@gmail.com>
2022-08-04 03:36:41 -0300

    Insercao das rotas no app de configuracao

--- src/app.js
+++ src/app.js
@@ -3,15 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Application routes
+import request from './routes/request';
+
 class App {
   constructor() {
     this.app = express();
     this.middlewares();
+    this.routes();
   }
 
   middlewares() {
     this.app.use(express.urlencoded({ extented: true }));
     this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', request);
   }
 }
 
